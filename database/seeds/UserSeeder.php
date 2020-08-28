95f7e171c30947c6b16fdd3057d1884e64d01b67
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2020-08-29 05:51:28 +0700

    back-end

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::unguard();
+        User::truncate();
+
+        User::create([
+            'name'              => "Administrator",
+            'email'             => "admin@gmail.com",
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+        ]);
+
+        factory(User::class,30)->create();
+        User::reguard();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+ });
+
+Route::post('/login', 'LoginController@index');
+Route::get('/logout', 'LoginController@logout');
 
 Route::resource('product', 'ProductController')->except('create','edit');
